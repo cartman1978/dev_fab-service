87c7b9ed6be5dca1a55790b45ba32c6a358ebaec
Fabio Torti <cartman_7802@yahoo.ie>
2021-10-09 21:39:39 +0100

    Added home app and templates

--- dev_fabio/urls.py
+++ dev_fabio/urls.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from django.contrib import admin
-from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
+    path('', include('home.urls')),
 ]
