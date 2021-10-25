24b5f3e6424c94b20c6b051a7490861613bff8db
Fabio Torti <cartman_7802@yahoo.ie>
2021-10-25 19:33:56 +0100

    addedjavascript functionality to open anc close the sidenav

--- static/js/main.js
+++ static/js/main.js
@@ -14,3 +14,21 @@ closeDropdown.addEventListener("click", () => {
 });
 
 // When hamburger menu is clicked show side nav bar
+let hamburger = document.querySelector(".hamburger");
+let navSide = document.querySelector(".side-nav");
+let closeNav = document.querySelector(".nav-close");
+let sideMenu = document.querySelector(".side-nav-menu");
+hamburger.addEventListener("click", () => {
+    navSide.classList.add("show-side-nav");
+    sideMenu.classList.add("side-menu-open");
+    sideMenu.classList.remove("side-menu-close");
+});
+
+closeNav.addEventListener("click", () => {
+    sideMenu.classList.remove("side-menu-open");
+    sideMenu.classList.add("side-menu-close");
+
+    setTimeout(() => {
+        navSide.classList.remove("show-side-nav");
+    }, 500);
+});
