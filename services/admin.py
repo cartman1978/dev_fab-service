70baccfe7432b038ad435cfb7df43d45010a045a
Fabio Torti <cartman_7802@yahoo.ie>
2021-11-07 19:18:22 +0000

    add product admin

--- services/admin.py
+++ services/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Category, Item
 
-# Register your models here.
+
+class ItemAdmin(admin.ModelAdmin):
+    list_display = (
+        "code",
+        "name",
+        "category",
+        "price",
+        "image"
+    )
+
+    ordering = (
+        "code",
+        )
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        "display_name",
+        "name",
+    )
+
+
+admin.site.register(Item, ItemAdmin)
+admin.site.register(Category, CategoryAdmin)
