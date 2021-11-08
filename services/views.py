e535c73ec508dfe1dae5797ccfc56f85b38d4ebe
Fabio Torti <cartman_7802@yahoo.ie>
2021-11-08 18:53:47 +0000

    added item view and template

--- services/views.py
+++ services/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Item
 
-# Create your views here.
+
+def all_items(request):
+    """ Get all the items page from the database.
+    Handles the search queries from navigation using
+    django Q object to filter the results.
+    """
+    
+    items = Item.objects.all()
+    
+    context = {
+        'item': items
+    }
+    
+    return render(request, 'services/services.html', context)
