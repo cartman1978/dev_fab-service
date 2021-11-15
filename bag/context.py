bcd5f962fdba1d8dafbd7be3af0f2b99826914ac
Fabio Torti <cartman_7802@yahoo.ie>
2021-11-15 07:52:30 +0000

    added logic to show bag item

--- bag/context.py
+++ bag/context.py
@@ -2,18 +2,30 @@ from django.conf import settings
 from django.shortcuts import get_object_or_404
 from services.models import Item
 from decimal import Decimal
-
+from services.models import Item
 
 def bag_contents(request):
     
     bag_items = []
     total = 0
-    service_count = 0
+    item_count = 0
+    bag = request.session.get('bag', {})
+    
+    for item_id, quantity in bag.items():
+        item = get_object_or_404(Item, pk=item_id)
+        total += quantity * item.price
+        item_count += quantity
+        bag_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'item': item,
+            
+        })
     
     context = {
         'bag_items': bag_items,
         'total': total,
-        'service_count': service_count,
+        'item_count': item_count,
     }
     
     return context
--- dev_fabio/settings.py
+++ dev_fabio/settings.py
@@ -159,7 +159,7 @@ STATIC_ROOT = ''
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = ( os.path.join('static'), )
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
