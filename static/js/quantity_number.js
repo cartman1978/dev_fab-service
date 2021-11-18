8d0297d40e221d7d99ba9d5e0afae2f96b6a5727
Fabio Torti <cartman_7802@yahoo.ie>
2021-11-18 08:21:40 +0000

    added quantity_number functionality to udpate quantity item

---
+++ static/js/quantity_number.js
@@ -0,0 +1,28 @@
+let increaseBtn = document.querySelectorAll(".plus-btn");
+let decreaseBtn = document.querySelectorAll(".minus-btn");
+increaseBtn.forEach((button) => {
+    button.addEventListener("click", (event) => {
+        let clickedBtn = event.target;
+        let inputContainer = clickedBtn.parentElement.parentElement.children[1];
+        let input = inputContainer.children[1].children[0];
+        let max = input.getAttribute("max");
+        let inputValue = input.value;
+        let newValue = parseInt(inputValue) + 1;
+        if (inputValue < max) {
+            input.value = newValue;
+        }
+    });
+});
+decreaseBtn.forEach((button) => {
+    button.addEventListener("click", (event) => {
+        let clickedBtn = event.target;
+        let inputContainer = clickedBtn.parentElement.parentElement.children[1];
+        let input = inputContainer.children[1].children[0];
+        let min = input.getAttribute("min");
+        let inputValue = input.value;
+        let newValue = parseInt(inputValue) - 1;
+        if (inputValue > min) {
+            input.value = newValue;
+        }
+    });
+});
