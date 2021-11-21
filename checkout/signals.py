db986f9a5e0fef9bf5ca534c8c150267441009a3
Fabio Torti <cartman_7802@yahoo.ie>
2021-11-21 20:06:16 +0000

    added forms, adming and signals

---
+++ checkout/signals.py
@@ -0,0 +1,19 @@
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+
+from .models import OrderLineItem
+
+
+@receiver(post_save, sender=OrderLineItem)
+def update_total_on_save(sender, instance, created, **kwargs):
+    ''' update total when lineitem is created or updated '''
+
+    instance.order.update_grand_total()
+
+
+@receiver(post_delete, sender=OrderLineItem)
+def update_total_on_delete(sender, instance, **kwargs):
+    ''' Update total when a lineitem is deleted '''
+
+    instance.order.update_grand_total()
+
