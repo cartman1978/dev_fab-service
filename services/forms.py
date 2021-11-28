65f589670c45c50ab07d47b88ceebdee7555e518
Fabio Torti <cartman_7802@yahoo.ie>
2021-11-28 17:58:32 +0000

    styled add_service template

--- services/forms.py
+++ services/forms.py
@@ -1,18 +1,25 @@
 from django import forms
 from .models import Item, Category
 
+
 class ItemForm(forms.ModelForm):
 
     class Meta:
         model = Item
-        fields = '__all__'
+        fields = "__all__"
+
+    image = forms.ImageField(
+        label="Image",
+        required=False,
         
+    )
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
         categories = Category.objects.all()
         display_names = [(c.id, c.get_display_name()) for c in categories]
 
-        self.fields['category'].choices = display_names
+        self.fields["category"].choices = display_names
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'border-black rounded-0'
