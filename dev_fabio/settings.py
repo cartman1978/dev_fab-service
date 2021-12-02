ac5eb5e234ac251d91a50ed1c928bc478b99f855
Fabio Torti <cartman_7802@yahoo.ie>
2021-12-02 18:34:37 +0000

    testing new aws media path

--- dev_fabio/settings.py
+++ dev_fabio/settings.py
@@ -63,6 +63,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -194,6 +195,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 SITE_ID = 1
+
 
 if 'USE_AWS' in os.environ:
     
