2aeaaf039f1bc693bd71ee5586a9fd461b0aced6
Fabio Torti <cartman_7802@yahoo.ie>
2021-12-04 16:25:45 +0000

    added send real email settings

--- dev_fabio/settings.py
+++ dev_fabio/settings.py
@@ -232,12 +232,12 @@ STRIPE_VH_SECRET = os.getenv('STRIPE_VH_SECRET', '')
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_EMAIL_ADDRESS = 'cartman_7802@yahoo.ie'
+    DEFAULT_FROM_EMAIL = 'devfab@example.com'
 else:
-    EMAIL_BACKEND = 'django.core.mail.backends.smpt.EmailBackend'
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
     EMAIL_PORT = 587
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
-    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
