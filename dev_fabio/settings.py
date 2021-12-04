eba4fc7b8d96dbc9a67ceecdf245e490520c411a
Fabio Torti <cartman_7802@yahoo.ie>
2021-12-04 16:07:27 +0000

    added send real email settings

--- dev_fabio/settings.py
+++ dev_fabio/settings.py
@@ -115,10 +115,6 @@ AUTHENTICATION_BACKENDS = [
 
 SITE_ID = 1
 
-
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
-
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
@@ -233,4 +229,15 @@ STRIPE_CURRENCY = 'eur'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_VH_SECRET = os.getenv('STRIPE_VH_SECRET', '')
-DEFAULT_EMAIL_ADDRESS = 'cartman_7802@yahoo.ie'
+
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_EMAIL_ADDRESS = 'cartman_7802@yahoo.ie'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smpt.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
