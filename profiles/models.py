7cc292cd18c196cded0b4364b2886cf61ce60935
Fabio Torti <cartman_7802@yahoo.ie>
2021-12-05 21:33:45 +0000

    testing new confirmation email

--- profiles/models.py
+++ profiles/models.py
@@ -36,3 +36,16 @@ def create_or_update_user_profile(sender, instance, created, **kwargs):
         UserProfile.objects.create(user=instance)
     # Existing users: just save the profile
     instance.userprofile.save()
+    
+
+
+
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    """
+    Create or update the user profile
+    """
+    if created:
+        UserProfile.objects.create(user=instance)
+    # Existing users: just save the profile
+    instance.userprofile.save()
