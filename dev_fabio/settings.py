81ec07dbf855846698ceedd249c8dfd6d37c5fcc
Fabio Torti <cartman_7802@yahoo.ie>
2021-12-06 18:13:49 +0000

    testing new gmail account to send email

--- dev_fabio/settings.py
+++ dev_fabio/settings.py
@@ -226,7 +226,7 @@ STANDARD_DELIVERY_PERCENTAGE = 10
 STRIPE_CURRENCY = 'eur'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
-STRIPE_VH_SECRET = os.getenv('STRIPE_VH_SECRET', '')
+STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
@@ -238,4 +238,4 @@ else:
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
-    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER' )
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
--- profiles/models.py
+++ profiles/models.py
@@ -4,6 +4,7 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 from django_countries.fields import CountryField
+from django.core.exceptions import ObjectDoesNotExist
 
 
 class UserProfile(models.Model):
@@ -22,6 +23,8 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return self.user.username
+    
+
 
 
 @receiver(post_save, sender=User)
