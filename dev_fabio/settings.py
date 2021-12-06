0390bedd24eba5694ecc26ce6de8c90fa2c1542a
Fabio Torti <cartman_7802@yahoo.ie>
2021-12-06 19:11:10 +0000

    testing new email app

--- dev_fabio/settings.py
+++ dev_fabio/settings.py
@@ -228,6 +228,7 @@ STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
+
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
     DEFAULT_FROM_EMAIL = 'devfab@example.com'
@@ -236,6 +237,6 @@ else:
     EMAIL_USE_TLS = True
     EMAIL_PORT = 587
     EMAIL_HOST = 'smtp.gmail.com'
-    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'gelatoallafragola.kt@gmail.com')
-    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS', 'ajnxsicyifjjlhit')
-    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER', 'gelatoallafragola.kt@gmail.com')
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
