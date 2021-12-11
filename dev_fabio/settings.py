ad73391f585aeef1327dbc9215b149aa8b71ea5d
Fabio Torti <cartman_7802@yahoo.ie>
2021-12-11 10:05:36 +0000

    replaced favicon img and changed top nav bg color

--- dev_fabio/settings.py
+++ dev_fabio/settings.py
@@ -7,7 +7,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-import builtins
+
 from pathlib import Path
 import os
 import dj_database_url
@@ -21,12 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'l6(eswe%)-76_6*wd211e#m1qbjgn!+0n$2k@rggi4+7kgte9(')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = "DEVELOPMENT" in os.environ
-DEBUG = True
-ALLOWED_HOSTS = ['devfab.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['devfab.herokuapp.com', 'localhost']
 
 
 # Application definition
